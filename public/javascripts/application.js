c0825b08b02403df40d51a170ee83643634ef805
Trevor Erwin <erwintj@mail.uc.edu>
2018-10-14 23:03:02 -0400

    Configured app to render tvml

---
+++ public/javascripts/application.js
@@ -0,0 +1,21 @@
+function getDocument(url) {
+	const templateXHR = new XMLHttpRequest();
+	templateXHR.responseType = "document";
+	templateXHR.addEventListener("load", function () { pushDoc(templateXHR.responseXML); }, false);
+	templateXHR.open("GET", url, true);
+	templateXHR.send();
+	return templateXHR;
+}
+
+function pushDoc(document) {
+	navigationDocument.pushDocument(document);
+}
+
+App.onLaunch = () => {
+	const templateURL = 'http://localhost:3000';
+	getDocument(templateURL);
+}
+
+App.onExit = () => {
+	console.log('App finished');
+}
